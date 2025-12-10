cbfbc1d5cbe928f3251708309af205e0cd76f04f
FenrisFox <jonas.vonaesch@gmail.com>
2025-12-10 10:31:58 +0100

    Added visual cue on use of the copy button

--- assets/js/copyable.js
+++ assets/js/copyable.js
@@ -22,8 +22,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 const codeEl = div.querySelector('code');
                 const textToCopy = codeEl ? codeEl.innerText : div.innerText;
                 await navigator.clipboard.writeText(textToCopy);
+
                 div.dataset.copied = 'true';
+                button.textContent = 'Copied'
                 setTimeout(() => delete div.dataset.copied, 1000);
+                setTimeout(() => button.textContent = "Copy", 2000);
+
             } catch (err) {
                 console.error('Failed to copy: ', err);
             }
